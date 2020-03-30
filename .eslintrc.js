5ea68ba07c03253dbce9049de7db78943d38555e
Carles Andrés <carlos.andres.c@gmail.com>
2020-03-30 10:57:49 +0100

    Proof of concept for incoming comms mechanism (#40)
    * Proof of concept for incoming comms mechanism
    * Improved security of top-down messaging mechanism

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
-	"extends": "origami-component"
+  "extends": "origami-component",
+  "rules": {
+    "indent": ["error", "tab"]
+  }
 };
--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 import messenger from './src/js/postMessenger';
 
+const handleReceivedMessage = event => {
+	console.log('event.origin', event.origin);
+	console.log('event.data', event.data);
+	if (event.origin === 'https://ft.com' && event.data) {
+		if (event.data.messageType === 'oAdsEmbed') {
+			window.oAdsEmbedData = event.data.body;
+		}
+	}
+};
+
 /*
 * Initialise oAds Embed library.
 * - looks for a collapse element in the iframe
@@ -34,6 +44,8 @@ const oAdsEmbed = {
 			document.body.addEventListener('touchmove', swipeHandler.bind(null));
 			document.body.addEventListener('touchend', swipeHandler.bind(null));
 		}
+
+		window.addEventListener('message', handleReceivedMessage, false);
 	}
 };
 
