78a19ab545ba912e77d44ac40ef788027c540a56
Jake Champion <me@jakechampion.name>
2020-09-04 11:38:03 +0100

    add extension to relative imports to make the code work directly in a browser
    relative imports require the full path including the extension because browsers use the import name to make a network request and they do not implicitly add a .js file extension if one is missing

--- test/main.test.js
+++ test/main.test.js
@@ -1,8 +1,8 @@
 /* eslint-env mocha */
 /* global proclaim sinon */
-import * as fixtures from './helpers/fixtures';
-import oAdsEmbed from '../main';
-import { dispatchTouchEvent } from './helpers/utils';
+import * as fixtures from './helpers/fixtures.js';
+import oAdsEmbed from '../main.js';
+import { dispatchTouchEvent } from './helpers/utils.js';
 
 describe('o-ads-embed', () => {
 	context('Window has loaded', () => {
